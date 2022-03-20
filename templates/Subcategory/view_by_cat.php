f9356a86b788f3bd66375c455c9b7a0aafca18f4
Jingyun Geng <jgen0005@student.monash.edu>
2022-03-20 12:54:10 +1000

    merge composer file

---
+++ templates/Subcategory/view_by_cat.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Category[]|\Cake\Collection\CollectionInterface $category
+ * @var \App\Model\Entity\Category[]|\Cake\Collection\CollectionInterface $subcategories
+ */
+$this->disableAutoLayout();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Simple Sidebar - Start Bootstrap Template</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.sidebar.css" rel="stylesheet" />
+</head>
+<body>
+<div class="d-flex" id="wrapper">
+    <!-- Sidebar-->
+    <div class="border-end bg-white" id="sidebar-wrapper">
+        <div class="sidebar-heading border-bottom bg-light">Categories
+            <div><?= $this->Html->link(__('New Category'), ['action' => 'add'], ['class' => 'button  float-right']) ?></div>
+        </div>
+        <div class="list-group list-group-flush">
+            <?php foreach ($category as $categories): ?>
+                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!"><?= h($categories->name) ?></a>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <div class="border-end bg-white" id="sidebar-wrapper">
+        <div class="sidebar-heading border-bottom bg-light">Subcategories
+            <div><?= $this->Html->link(__('New Category'), ['action' => 'add'], ['class' => 'button  float-right']) ?></div>
+        </div>
+        <div class="list-group list-group-flush">
+            <?php foreach ($categories as $category): ?>
+                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!"><?= h($category->name) ?></a>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <!-- Page content-->
+    <div class="container-fluid">
+        <h1 class="mt-4">Simple Sidebar</h1>
+        <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
+        <p>
+            Make sure to keep all page content within the
+            <code>#page-content-wrapper</code>
+            . The top navbar is optional, and just for demonstration. Just create an element with the
+            <code>#sidebarToggle</code>
+            ID which will toggle the menu when clicked.
+        </p>
+    </div>
+</div>
+</div>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.sidebar.js"></script>
+</body>
+</html>
+
