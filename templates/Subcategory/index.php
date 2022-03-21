b7c966364ccb5e09dc00f4b27441b03c640e5e4a
DESKTOP-A9S1C0I\Harry <gche0005@student.monash.edu>
2022-03-21 13:40:08 +0800

    subcategory

--- templates/Subcategory/index.php
+++ templates/Subcategory/index.php
@@ -1,3 +1,8 @@
+
+
+
+
+
 <?php
 /**
  * @var \App\View\AppView $this
@@ -5,21 +10,34 @@
  */
 ?>
 <div class="subcategory index content">
-    <?= $this->Html->link(__('New Subcategory'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Subcategory') ?></h3>
-    <div class="table-responsive">
-        <table>
+    <?php
+
+    $this->disableAutoLayout();
+    echo $this->Html->css('fresh-bootstrap-table.css');
+    ?>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Roboto:400,700,300" rel="stylesheet" type="text/css">
+    <h1 class="text-center"><?= __('Subcategory') ?></h1>
+    <div class="fresh-table full-color-azure" style>
+        <div class="container">
+            <h4 class="text-right">
+                <?= $this->Html->link(__('New Subcategory'), ['action' => 'add'], ['class' => 'button float-right']) ?> 
+            </h4>
+
+            <table id="fresh-table" class="bootstrap-table">
             <thead>
                 <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('id_cat') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
+                    <th  scope="col"><?= ('ID') ?></th>
+                    <th  scope="col"><?= __('Name') ?></th>
+                    <th  scope="col"><?= __('ID_Cat') ?></th>
+                    <th class="actions"><?= __('Actions')?></th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody>   
                 <?php foreach ($subcategory as $subcategory): ?>
-                <tr>
+                <tr scope = "row">
                     <td><?= $this->Number->format($subcategory->id) ?></td>
                     <td><?= h($subcategory->name) ?></td>
                     <td><?= $subcategory->has('category') ? $this->Html->link($subcategory->category->name, ['controller' => 'Category', 'action' => 'view', $subcategory->category->id]) : '' ?></td>
@@ -44,3 +62,30 @@
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
 </div>
+    <!-- Javascript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table/dist/bootstrap-table.min.js"></script>
+
+    <script type="text/javascript">
+        var $table = $('#fresh-table')
+        var $alertBtn = $('#alertBtn')
+
+        $(function () {
+            $table.bootstrapTable({
+                classes: 'table table-hover table-striped',
+                toolbar: '.toolbar',
+
+                search: true,
+                showRefresh: false,
+                showToggle: true,
+                showColumns: false,
+                pagination: true,
+                striped: true,
+                sortable: true,
+                pageSize: 8,
+                pageList: [8, 10, 25, 50, 100],
+            })
+
+        })
+
+    </script>
--- templates/Users/index.php
+++ templates/Users/index.php
@@ -1,7 +1,3 @@
-
-
-
-
 <?php
 /**
  * @var \App\View\AppView $this
