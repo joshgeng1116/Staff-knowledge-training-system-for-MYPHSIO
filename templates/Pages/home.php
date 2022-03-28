25e1de706854051cbe4bd2c461024c07cfc57cd0
DESKTOP-A9S1C0I\Harry <gche0005@student.monash.edu>
2022-03-28 16:06:34 +0800

    change home page and add a logout button

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -3,91 +3,32 @@ $this->disableAutoLayout();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<link rel="icon" type="image/x-icon" href="assets/myphysio.ico" />
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>MyPhysio</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/myphysio.ico" />
-        <!-- Bootstrap icons-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/home.css" rel="stylesheet" />
+        <title>Staff Page</title>
+        <link href="css/staffindex.css" rel="stylesheet">
     </head>
-    <body> 
-        <!-- Navigation-->
-        <!-- Masthead-->
-        <img class="img-logo" src="assets/img/logo.png" alt="my logo">
-        <header class="masthead">
-            <div class="container position-relative">
-                <div class="row justify-content-center">
-                    <div class="col-xl-6">
-                        <div class="text-center text-white">
-                            <!-- Page heading-->
-                            <h1 class="mb-5">MyPhysio</h1>
-                            <h1 class="mb-5">Staff</h1>
-                            <!-- Signup form-->
-                            <!-- * * * * * * * * * * * * * * *-->
-                            <!-- * * SB Forms Contact Form * *-->
-                            <!-- * * * * * * * * * * * * * * *-->
-                            <!-- This form is pre-integrated with SB Forms.-->
-                            <!-- To make this form functional, sign up at-->
-                            <!-- https://startbootstrap.com/solution/contact-forms-->
-                            <!-- to get an API token!-->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- Icons Grid-->
-        <section class="features-icons bg-light text-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-window m-auto text-primary"></i></div>
-                            <h3><a href="<?=$this->Url->build(['controller'=>'TrainingPlan','action'=>'staffindex'])?>">Training Module</a></h3>
-</br>                         
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-tag m-auto text-primary"></i></div>
-                            <h3><a href="<?=$this->Url->build(['controller'=>'Category','action'=>'index'])?>">Knowledge Management</a></h3>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-calendar m-auto text-primary"></i></div>
-                            <h3><a href= "#">Calendar</a></h3>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-app m-auto text-primary"></i></div>
-                            <h3><a href= "#">Leave Application</a></h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Image Showcases-->
-        <!-- Testimonials-->
-        <!-- Call to Action-->
-        <!-- Footer-->
-
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <!-- * *                               SB Forms JS                               * *-->
-        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
-    </body>
+<body>
+       <div class="banner">
+           <div class="navbar">
+               <img src="assets/img/logo1.png" calss="logo">
+               <ul>
+                   <li><a href="#">Home</a></li>
+                   <li><a href="#">Something</a></li>
+                   <li><a href="#">Something</a></li>
+                   <li><a href="#">Something</a></li>
+                   <li><a href="users/logout">LOGOUT</a></li>
+               </ul>
+           </div>
+           <div class="content">
+               <h1>MY PHYSIO</h1>
+               <div>
+                <a href="training-plan"><button type="button"><span></span>TRAINING</button></a>
+                <a href="category"><button type="button"><span></span>DOCUMENT</button></a>
+                <a href="#"><button type="button"><span></span>CALENDAR</button></a>
+                <a href="#"><button type="button"><span></span>LEAVE APP</button></a>
+               </div>
+           </div>
+       </div> 
+</body>
 </html>
