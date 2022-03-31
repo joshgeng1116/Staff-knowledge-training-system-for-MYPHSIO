d7a74e26d9b22a81b1d6641d96cda335bc60245a
Disha Patel <dishapatel@Dishas-MacBook-Pro.local>
2022-03-31 12:54:53 +1100

    homepage links updated

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -14,19 +14,19 @@ $this->disableAutoLayout();
                <img src="assets/img/logo1.png" calss="logo">
                <ul>
                    <li><a href="#">Home</a></li>
-                   <li><a href="#">Something</a></li>
-                   <li><a href="#">Something</a></li>
-                   <li><a href="#">Something</a></li>
+                   <li><a href="training-plan/staffindex">Training Plan</a></li>
+                   <li><a href="category">Documents</a></li>
+                   <li><a href="leaves">Leave</a></li>
                    <li><a href="users/logout">LOGOUT</a></li>
                </ul>
            </div>
            <div class="content">
                <h1>MY PHYSIO</h1>
                <div>
-                <a href="training-plan"><button type="button"><span></span>TRAINING</button></a>
+                <a href="training-plan/staffindex"><button type="button"><span></span>TRAINING</button></a>
                 <a href="category"><button type="button"><span></span>DOCUMENT</button></a>
                 <a href="#"><button type="button"><span></span>CALENDAR</button></a>
-                <a href="#"><button type="button"><span></span>LEAVE APP</button></a>
+                <a href="leaves"><button type="button"><span></span>LEAVE </button></a>
                </div>
            </div>
        </div> 
