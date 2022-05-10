57bba890eced5a8113287c2b84916bc667ef9ce1
DESKTOP-A9S1C0I\Harry <gche0005@student.monash.edu>
2022-05-10 17:05:02 +0800

    123

--- templates/Events/json/eventstaff.php
+++ templates/Events/json/eventstaff.php
@@ -13,7 +13,7 @@ foreach ($leaves as $leave){
         foreach ($users as $user){
             if($leave->user_id == $user->id){
                 $item = [
-                    'title'=> $user->name,
+                    'title'=> $user->name.' leave',
                     'start' => $leave->date_start,
                     'end' => $leave->date_end
                 ];
@@ -21,5 +21,16 @@ foreach ($leaves as $leave){
             }
         }
     }
+}
+foreach ($users as $user){
+    $month = date("d",strtotime($user->date_of_birth));
+        $date = date("m",strtotime($user->date_of_birth));
+        $year = date('Y');
+        $item = [
+            'title' => $user->name." Birthday",
+            'start' => $year.'-'.$month.'-'.$date,
+            'end' => $year.'-'.$month.'-'.$date
+        ];
+        $data[] = $item;
 }
 echo json_encode($data);
