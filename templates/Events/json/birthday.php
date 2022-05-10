f8c2f24d66d9bf2fd53f9bf8c6eaa4eba06dbb81
Jingyun Geng <jgen0005@student.monash.edu>
2022-05-10 18:48:09 +1000

    a

--- templates/Events/json/birthday.php
+++ templates/Events/json/birthday.php
@@ -2,10 +2,13 @@
 $data = [];
 foreach ($events as $event){
     if($event->type == 1) {
+        $month = date("d",strtotime($event->start_date));
+        $date = date("m",strtotime($event->start_date));
+        $year = date('Y');
         $item = [
             'title' => $event->name,
-            'start' => $event->start_date,
-            'end' => $event->end_date
+            'start' => $year.'-'.$month.'-'.$date,
+            'end' => $year.'-'.$month.'-'.$date
         ];
         $data[] = $item;
     }
