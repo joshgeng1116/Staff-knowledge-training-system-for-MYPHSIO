628cdf8f05f7ab5cea5cc8a0479058aa5a45c702
Jingyun Geng <jgen0005@student.monash.edu>
2022-05-23 15:38:06 +1000

    doc

--- templates/Documents/taskdoc.php
+++ templates/Documents/taskdoc.php
@@ -18,15 +18,12 @@ echo $this->Html->css('styles.sidebar.css');
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Core theme CSS (includes Bootstrap)-->
 </head>
-<body>
-<div class="d-flex" id="wrapper">
+
     <!-- Page content-->
         <a herf="<?= $document->path?>" target="_blank"></a>
         <body>
         <iframe src="https://review.u21s2102.monash-ie.me/<?= h($document->path)?>" width="100%" height="100%"></iframe>
         </body>
-</div>
-</div>
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
